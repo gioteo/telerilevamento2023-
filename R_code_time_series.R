969b15acf68ebda205db230155720eca39fd2102
gioteo <127323347+gioteo@users.noreply.github.com>
2023-03-31 17:30:00 +0200

    Update R_code_time_series.R

--- R_code_time_series.R
+++ R_code_time_series.R
@@ -20,3 +20,15 @@ rlist
 
 import <- lapply(rlist, raster)
 import
+
+TGr <- stack(import)
+TGr
+
+plot(TGr)
+
+plotRGB(TGr, r = 1, g = 2, b = 3, stretch = "lin")                       
+plotRGB(TGr, r = 2, g = 3, b = 4, stretch = "lin")                       
+
+dift <- TGr[[2]] - TGr[[1]]
+cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
+plot(TGr, col=cl)
