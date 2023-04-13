72e80e6bc79ec394d60db416e03b7855029eeb0e
gioteo <127323347+gioteo@users.noreply.github.com>
2023-04-13 17:07:02 +0200

    Update R_code_time_series.R

--- R_code_time_series.R
+++ R_code_time_series.R
@@ -35,3 +35,34 @@ plot(TGr, col=cl)
 
 
 # Nuovo studio. scaricato i dati della cartela En per fare una analisi multitemporale delle emissioni di ossidi di azoto in europa
+
+# lezione 13 aprile    Exercise 2 
+library (raster)
+setwd("C:/lab/EN")
+
+# importing a file 
+
+en_first <- raster("EN_0001.png")
+en_first
+
+cl <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(en_first, col=cl)
+
+rlist <- list.files(pattern="EN") # faccio una lista 
+rlist
+
+import <- lapply(rlist, raster) # importiamo tutti e i 13 file 
+import
+
+TGr <- stack(import)  # comprimo tutti e i 13 file 
+TGr
+
+plot(TGr, col=cl)
+
+# check
+
+par(mfrow=c(1,2))
+plot(en_first, col=cl)
+plot(EN[[1]], col=cl)
+
+
