0ada3a30070581a66073c37479a6aedd16677f7e
gioteo <127323347+gioteo@users.noreply.github.com>
2023-05-11 17:48:39 +0200

    Create R_code_Copernicus.R

---
+++ R_code_Copernicus.R
@@ -0,0 +1,9 @@
+# R code for dowloding and visualizing Copernicus data 
+
+install.packages("ncdf4")
+library(ncdf4)
+library(ratser)
+
+setwd("C:/lab/")
+
+# dataset: c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc  
