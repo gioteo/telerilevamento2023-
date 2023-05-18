d4a0322df5bca64b33b64362fcd0ea06a81b6413
gioteo <127323347+gioteo@users.noreply.github.com>
2023-05-18 18:05:23 +0200

    Update R.code.sdm.R

--- R.code.sdm.R
+++ R.code.sdm.R
@@ -4,3 +4,15 @@ install.packages("sdm")
 library(sdm)
 
 library(rgdal) # Species
+
+library(raster) # predictors
+library(rgdal) # species
+
+file <- system.file("external/species.shp", package="sdm")
+
+file   # serve a vedere il pecorso per avere i dati
+
+species <- shapefile(file)
+species
+
+# "occorenza" è la presenza o meno di una specie,   1 è presente 0 è assente 
