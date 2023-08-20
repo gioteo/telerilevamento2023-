2615a85d8673d9c9f4f82d06ebc36eb6f5eb4017
gioteo <127323347+gioteo@users.noreply.github.com>
2023-08-20 12:08:21 +0200

    Update R_code_time_series.R

--- R_code_time_series.R
+++ R_code_time_series.R
@@ -1,94 +1,18 @@
 # R code for importing and anylizing several images
 
-library(raster)
-setwd("C:/lab/greenland")
-
-lst_2000 <- raster("lst_2000.tif")
-plot(lst_2000)
-
-#exercise: import all the data
-
-lst2005 <- raster("lst_2005.tif")
-lst2010 <- raster("lst_2010.tif")
-lst2015 <- raster("lst_2015.tif")
-
-ls()
-
-
-rlist <- list.files(pattern="lst")
-rlist
-
-import <- lapply(rlist, raster)
-import
-
-TGr <- stack(import)
-TGr
-
-plot(TGr)
-
-plotRGB(TGr, r = 1, g = 2, b = 3, stretch = "lin")                       
-plotRGB(TGr, r = 2, g = 3, b = 4, stretch = "lin")                       
-
-dift <- TGr[[2]] - TGr[[1]]
-cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
-plot(TGr, col=cl)
-
-
-# Nuovo studio. scaricato i dati della cartela En per fare una analisi multitemporale delle emissioni di ossidi di azoto in europa
-
-# lezione 13 aprile    Exercise 2 
-library (raster)
-setwd("C:/lab/EN")
-
-# importing a file 
-
-en_first <- raster("EN_0001.png")
-en_first
-
-cl <- colorRampPalette(c('red','orange','yellow'))(100)
-plot(en_first, col=cl)
-
-rlist <- list.files(pattern="EN") # faccio una lista 
-rlist
-
-import <- lapply(rlist, raster) # importiamo tutti e i 13 file 
-import
-
-TGr <- stack(import)  # comprimo tutti e i 13 file 
-TGr
-
-plot(TGr, col=cl)
-
-# check
-
-par(mfrow=c(1,2))
-plot(en_first, col=cl)
-plot(EN[[1]], col=cl)
-
-
-
-
-
-
-
-
-AAAAAAAAAAAAA
-
-
 # Time series analysis
 # Greenland increase of temperature
-# Data and code from Emanuela Cosma
 
 # install.packages("raster")
 # install.packages("rasterVis")
 library(raster)
-library(rasterVis) 
+# library(rasterVis)
 
 # library(rgdal)
 
-setwd("~/lab/greenland") # Linux
-# setwd("C:/lab/greenland") # Windows
-# setwd("/Users/name/Desktop/lab/greenland") # Mac 
+ setwd("C:/lab/greenland") # Windows
+
+
 
 lst_2000 <- raster("lst_2000.tif")
 lst_2005 <- raster("lst_2005.tif")
@@ -113,18 +37,79 @@ TGr <- stack(import)
 TGr
 plot(TGr)
 
-plotRGB(TGr, 1, 2, 3, stretch="Lin") 
-plotRGB(TGr, 2, 3, 4, stretch="Lin") 
-plotRGB(TGr, 4, 3, 2, stretch="Lin") 
+plotRGB(TGr, 1, 2, 3, stretch="Lin")
+plotRGB(TGr, 2, 3, 4, stretch="Lin")
+plotRGB(TGr, 4, 3, 2, stretch="Lin")
 
-levelplot(TGr)
+# levelplot(TGr)
 cl <- colorRampPalette(c("blue","light blue","pink","red"))(100)
-levelplot(TGr, col.regions=cl)
+plot(TGr, col=cl)
 
-levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+# levelplot(TGr,col.regions=cl, names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+#
+# levelplot(TGr,col.regions=cl, main="LST variation in time",
+#           names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+#
 
-levelplot(TGr,col.regions=cl, main="LST variation in time",
-          names.attr=c("July 2000","July 2005", "July 2010", "July 2015"))
+##########################
+#### Example 2: NO2 decrease during the lockdown period
+##########################
+
+library(raster)
+
+setwd("~/lab/en") # Linux
+# setwd("C:/lab/en") # Windows
+# setwd("/Users/name/Desktop/lab/en") # Mac
+
+en01 <- raster("EN_0001.png")
+
+cl <- colorRampPalette(c('red','orange','yellow'))(100) #
+plot(en01, col=cl)
+
+en13 <- raster("EN_0013.png")
+plot(en13, col=cl)
+
+# Let's import the whole set (altogether!)
+
+# Exercise: import the whole as in the Greenland example
+# by the following steps: list.files, lapply, stack
+
+rlist <- list.files(pattern="EN")
+
+# lapply(X,FUN)
+rimp <- lapply(rlist, raster)
+
+# stack
+en <- stack(rimp)
+
+# plot everything
+plot(en, col=cl)
+
+# Exercise: plot EN01 besides EN13
+par(mfrow=c(1,2))
+plot(en[[1]], col=cl)
+plot(en[[13]], col=cl)
+
+# or:
+en113 <- stack(en[[1]], en[[13]])
+plot(en113, col=cl)
+
+# let's make the difference:
+difen <-  en[[1]] - en[[13]]
+cldif <- colorRampPalette(c('blue','white','red'))(100) #
+plot(difen, col=cldif)
+
+# plotRGB of three files together
+plotRGB(en, r=1, g=7, b=13, stretch="lin")
+plotRGB(en, r=1, g=7, b=13, stretch="hist")
+
+
+
+
+
+
+
+
 
 
 
