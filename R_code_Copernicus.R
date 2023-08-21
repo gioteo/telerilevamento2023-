7de72aa76d5b528b7f8f15a9152c19ff4ff06731
gioteo <127323347+gioteo@users.noreply.github.com>
2023-08-21 16:21:43 +0200

    Update R_code_Copernicus.R

--- R_code_Copernicus.R
+++ R_code_Copernicus.R
@@ -10,6 +10,7 @@ setwd("C:/lab/")
 
 
 sc <-raster("c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
+sc
 plot(sc)
 
 # with the raster package -> RasterLayer
@@ -39,4 +40,36 @@ names(sc.crop.d)
 ggplot() +                                            
 geom_raster(sc.crop.d, mapping = aes(x=x, y=y, fill = Surface.Soil.Moisture))+
 ggtitle("Surface.Soil.Moisture")  
+
+
+
+
+
+
+Forse fatta meglio
+
+library(raster)
+
+install.packages("ncdf4")
+library(ncdf4)
+library(ggplot2)
+library(viridis)
+
+# register and download data from:
+# https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
+
+ssoil <- raster("~/Downloads/c_gls_SSM1km_202305090000_CEURO_S1CSAR_V1.2.1.nc")
+
+ssoil
+
+plot(ssoil)
+
+ggplot() +
++ geom_raster(ssoil, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture))
+
+# with viridis
+ggplot() +
+geom_raster(ssoil, mapping=aes(x=x, y=y, fill=Surface.Soil.Moisture)) +
+scale_fill_viridis(option="magma")
+
 
